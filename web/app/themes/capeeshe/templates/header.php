93e285fe2e9cd8bd63e14b9ea825e4bc3c560916
Venom (Mel Macaluso) <capeeshe@gmail.com>
2018-04-07 19:11:28 +0100

    Remove placeholder header message

--- web/app/themes/capeeshe/templates/header.php
+++ web/app/themes/capeeshe/templates/header.php
@@ -5,15 +5,3 @@
 	<img src="<?= get_template_directory_uri() ?>/assets/images/logo.svg" />
 
 </header>
-<div id="diocane">
-	{{ message }}
-</div>
-
-<script>
-	var app = new Vue({
-		el: '#diocane',
-		data: {
-			message: 'Hello Vue!'
-		}
-	}) 
-</script>
